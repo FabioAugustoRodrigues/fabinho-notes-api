0d1b082a8dd9891ddc1020504342c63d74b40b3c
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2023-02-07 20:24:13 -0300

    feat: implement note repository in provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Note;
+use App\Repositories\Note\NoteRepositoryEloquent;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('App\Repositories\Note\NoteRepositoryInterface', 'App\Repositories\Note\NoteRepositoryEloquent');
+        $this->app->bind('App\Repositories\Note\NoteRepositoryInterface', function(){
+            return new NoteRepositoryEloquent(new Note());
+        });
     }
 
     /**
